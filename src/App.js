f3610747e40f10491c8b66c96057a510fe2eafa2
piyaphorn <piyaphorn.se@ksu.ac.th>
2024-08-27 16:37:08 +0700

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Button.js 	new file:   src/Mycss.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+
 
 function App() {
+  const [count, setCount] = setCount(10);
+  const name = "Hello";
+  const employees = [
+    { name: "AAA", email: "aaaa@gmail.com", age: 19},
+    { name: "BBB", email: "bbbb@gmail.com", age: 18},
+    { name: "CCC", email: "cccc@gmail.com", age: 20}
+  ]
+  console.log(employees);
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <Button onCilck={() => setCount(count + 1)}>Count ++</Button>
+      {employees.map((em, index) => (
+        <li key={index}>
+          ชื่อพนักงาน: {em.name} | อีเมล์: {em.email} | อายุ: {em.age}
+        </li>
+      ))}
+      <h1>Hello</h1>
+      <Button text = "OK"/>
+      <Button text = "Cancel"/>
+ 
     </div>
+    </>
+
   );
 }
 
